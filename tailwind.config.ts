938a83d9e045388b8c980ef73f6e60728c5b9915
Aditya Dhara <dev@adityadhara.com>
2024-03-12 01:23:14 -0700

    Scaffolding for tailwindcss 🔨

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
